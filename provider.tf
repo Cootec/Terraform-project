a42a8ed2fb92676c68a5739c0fccf1543c39b7e1
Cootec <99465020+Cootec@users.noreply.github.com>
2024-07-17 14:32:42 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -5,12 +5,6 @@ terraform {
       version = "5.30.0"
     }
   }
-
-  # # adding s3 bucket for remote state storage
-  # backend "s3"{
-  #   bucket = "tf-state-eks-test-tf"
-  #   region = "ap-south-1"
-  # }
 }
 
 provider "aws" {
